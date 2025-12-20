759288c98b8fc4bfa687a4a65b04ec0bf975c349
unknown <sithunaing8209@gmail.com>
2025-12-20 16:15:33 +0630

    main page done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,20 @@
 "use client"
 
+import { nanum } from "@/util/font";
 import { signIn } from "next-auth/react";
 
 
 const DashboardPage = () => {
     
     return (
-        <div className="flex justify-center items-center bg-indigo-800 h-screen w-screen">
-            <button onClick={() => signIn("google")} className="bg-blue-400 px-5 py-2 rounded text-xl m-5 cursor-pointer focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-700 ">Sign In</button>
+        <div className="flex flex-col justify-center items-center bg-secondary h-screen w-screen">
+            <div className="flex flex-col gap-10 bg-foodBg px-10 py-5 rounded-2xl">
+                <div>
+                    <p className={nanum.className + " text-primary text-[clamp(20px,3.5vw,38px)] [-webkit-text-stroke:1px] text-center"} >Mala Maocai Admin</p>
+                    <p className={nanum.className + " text-primary text-[clamp(18px,2.5vw,33px)] [-webkit-text-stroke:1px] text-center"} >Sign In</p>
+                </div>
+                <button onClick={() => signIn("google")} className="bg-primary text-secondary px-5 py-2 rounded text-xl m-5 cursor-pointer focus:outline-2 focus:outline-offset-2 focus:outline-primary active:bg-primary ">Sign In</button>
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,82 @@
+import { nanum , abyssinica } from "@/util/font";
+import { QrCodeIcon } from "@heroicons/react/16/solid";
+import { BookOpenIcon, ClipboardDocumentCheckIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
 import { connection } from "next/server";
+import { ComponentType, SVGProps } from "react";
 
 
 export default async function Home() {
   await connection();
   return (
-   <div>hello</div>
+   <div className="relative w-screen h-screen bg-secondary flex flex-col justify-between overflow-y-auto">
+      <Image alt="wave photo" src={"/wave.svg"} width={1000} height={1000} className="w-screen h-auto" />
+      <p className={nanum.className + " absolute text-secondary top-8 left-13 text-[clamp(20px,3.5vw,58px)] [-webkit-text-stroke:1px]"} >Mala Maocai</p>
+      <Image alt="Chef cartoon" src={"/chinese-Chef.png"} width={800} height={800} className="absolute top-3 right-10 w-[20vw] h-auto" />
+      <div className="flex justify-center items-center gap-5 py-10" >
+        <Link href={"/main?tableId=69456b57067d64620431326f"} >
+          <button className={abyssinica.className + " bg-primary px-3 py-2 rounded-md text-2xl text-secondary cursor-pointer hover:bg-primary/80" } >Order</button>
+        </Link>
+        <Link href={"/dashboard"} >
+          <button className={abyssinica.className + " bg-primary px-3 py-2 rounded-md text-2xl text-secondary cursor-pointer hover:bg-primary/80" } >Dashboard</button>
+        </Link>
+      </div>
+      <div className="flex justify-evenly items-center p-5">
+        {prons.map(item => (
+          <div key={item.id} className="flex flex-col items-center gap-2">
+            <item.icon className="w-25 text-primary" />
+            <p className={nanum.className + " text-[clamp(16px,1.6vw,30px)] text-center text-primary [-webkit-text-stroke:0.6px] "} >{item.firstPara}</p>
+            <p className={nanum.className + " text-[clamp(16px,1.6vw,30px)] text-center text-primary [-webkit-text-stroke:0.6px]"} >{item.secondPara}</p>
+          </div>
+        ))}
+      </div>
+      <div className="bg-[#602727] flex justify-between items-center text-secondary px-10 py-3">
+        <div>
+          <p className={nanum.className + " text-[clamp(16px,1.6vw,28px) italic"} >19 * 81 street</p>
+          <p className={nanum.className + " text-[clamp(16px,1.6vw,28px) italic"} >Aung Myae Thar Zan</p>
+          <p className={nanum.className + " text-[clamp(16px,1.6vw,28px) italic"} >contact@maocai.com</p>
+          <p className={nanum.className + " text-[clamp(16px,1.6vw,28px) italic"} >+95 123 456 78</p>
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <Link href={"/main?tableId=69456b57067d64620431326f"} >
+            <p className={nanum.className + " text-[clamp(16px,1.6vw,28px) italic underline underline-offset-3 cursor-pointer"} >Order</p>
+          </Link>
+          <Link href={"/dashboard"} >
+            <p className={nanum.className + " text-[clamp(16px,1.6vw,28px) italic underline underline-offset-3 cursor-pointer"} >Dashboard</p>
+          </Link>
+        </div>
+      </div>
+   </div>
   );
 }
+
+interface PronType {
+  id : number;
+  icon : ComponentType<SVGProps<SVGSVGElement>>;
+  firstPara : string;
+  secondPara : string;
+}
+
+const prons : PronType[] = [
+  {
+    id : 0,
+    icon : QrCodeIcon,
+    firstPara : "Easily manage your menus with",
+    secondPara : "Mala Maocai",
+  },
+  {
+    id : 1,
+    icon : BookOpenIcon,
+    firstPara : "Scan and order.Quick and easy!",
+    secondPara : "Your customer will love it!",
+  },
+  {
+    id : 2,
+    icon : ClipboardDocumentCheckIcon,
+    firstPara : "Dashboard and order apps are include ",
+    secondPara : "in every subscribtion",
+  },
+  
+]
